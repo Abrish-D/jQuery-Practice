6b80c6837af0bcc551a5df44eb01a3fa330e78b4
Abrham Desalegn <desalegnabrham961@gmail.com>
2024-02-12 21:00:53 +0300

    First commit

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// // for question 1
+// var paragragh = $("#sample1");
+// console.log(paragragh);
+// console.log(paragragh.text());
+
+// for question 2
+console.log($("#techCompanies"));
+console.log($("#techCompanies").children());
+console.log($(".red"));
+
+console.log(
+  ul.innerHTML += `<li> Facebook </li>`
+);
